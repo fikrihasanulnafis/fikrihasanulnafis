8ddd5368667b8eb94b4d0f86b9b9b5bc0cac6dba
fikrihasanulnafis <144106844+fikrihasanulnafis@users.noreply.github.com>
2023-09-05 15:19:28 +0700

    Add files via upload

---
+++ Konversi farenheit ke celsius.py
@@ -0,0 +1,3 @@
+Farenheit = int(input("Masukkan suhu fahrenheit"))
+Hitung = Farenheit*5/9+32
+print("Hasil konversi ke Celsius",Hitung)
---
+++ mengonversi suhu dalam derajat menjadi celsius.py
@@ -0,0 +1,3 @@
+Celsius = int(input("Masukkan suhu Celsius"))
+Hitung = Celsius*9/5+32
+print("Hasil konversi ke Fahrenheit",Hitung)
